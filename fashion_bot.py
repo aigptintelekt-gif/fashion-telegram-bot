2f2b50c509c6ba334cc2b1da1a173f9e629549fe
aigptintelekt-gif <aigptintelekt@gmail.com>
2026-01-30 11:05:48 +0300

    fashion_bot.py

--- fashion_bot.py
+++ fashion_bot.py
@@ -73,7 +73,7 @@ def _simple_text_gen(messages):
     """Универсальная функция для общения с LLM"""
     try:
         res = client.chat.completions.create(
-            model="qwen-vl-max",  # Или ваша актуальная модель в DashScope
+            model="qwen2.5-omni-7b",  # Или ваша актуальная модель в DashScope
             messages=messages
         )
         return res.choices[0].message.content
